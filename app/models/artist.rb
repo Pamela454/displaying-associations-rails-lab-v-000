ec4cc4567b8d91ae90dcbc230b641d03528bbcb6
Pamela Frieze <pfrieze@gmail.com>
2018-10-26 21:08:06 +0000

    automatically backed up by learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,7 +1,13 @@
 class Artist < ActiveRecord::Base
+<<<<<<< HEAD
   has_many :songs
 
   def song_count
     
+=======
+  has_many :songs 
+
+  def song_count
+>>>>>>> bc6c1735de92103f58a1ad21d1724f1978f9d428
   end
 end
